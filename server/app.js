6efb51b0e4fd286b1e3216234a478b2215aaaa91
Kaiya Walker <kaiyawalker@mail.com>
2018-08-28 16:37:24 -0700

    new env port

--- server/app.js
+++ server/app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-// mongoose.connect('mongodb://localhost/my-blog', { useMongoClient: true });
-mongoose.connect('mongodb://heroku_6drddcwl:opt28a9k1ov3u4vdfren627n2a@ds131432.mlab.com:31432/heroku_6drddcwl', { useMongoClient: true });
+mongoose.connect('mongodb://localhost/my-blog', { useMongoClient: true });
+// mongoose.connect('mongodb://heroku_6drddcwl:opt28a9k1ov3u4vdfren627n2a@ds131432.mlab.com:31432/heroku_6drddcwl', { useMongoClient: true });
 mongoose.Promise = Promise;
 
 const app = express();
