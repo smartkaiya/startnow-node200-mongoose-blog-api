5e9407e6134d01258373d0a7acfe2386da096be2
Kaiya Walker <kaiyawalker@mail.com>
2018-08-28 15:30:40 -0700

    new mongodb url

--- server/app.js
+++ server/app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-mongoose.connect('mongodb://localhost/my-blog', { useMongoClient: true });
+// mongoose.connect('mongodb://localhost/my-blog', { useMongoClient: true });
+mongoose.connect('mongodb://heroku_6drddcwl:opt28a9k1ov3u4vdfren627n2a@ds131432.mlab.com:31432/heroku_6drddcwl', { useMongoClient: true });
 mongoose.Promise = Promise;
 
 const app = express();
@@ -10,7 +11,7 @@ const app = express();
 
  app.get('/',(req, res) =>{
      res.status(200).send();
- });
+ })
  app.use('/api/users', require('./routes/users'));
 
  app.use('/api/blogs', require('./routes/blogs'));
